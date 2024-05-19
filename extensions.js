1f16c8d1412755e33bb2c35a61cceef6a27b5da6
YRS <yannickromainsegaar@gmail.com>
2024-05-19 14:13:25 +0200

    GoTours branch

--- extensions.js
+++ extensions.js
@@ -1,85 +1,108 @@
 const SVG_Thumb = `<svg width="24px" height="24px" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M5.29398 20.4966C4.56534 20.4966 4 19.8827 4 19.1539V12.3847C4 11.6559 4.56534 11.042 5.29398 11.042H8.12364L10.8534 4.92738C10.9558 4.69809 11.1677 4.54023 11.4114 4.50434L11.5175 4.49658C12.3273 4.49658 13.0978 4.85402 13.6571 5.48039C14.2015 6.09009 14.5034 6.90649 14.5034 7.7535L14.5027 8.92295L18.1434 8.92346C18.6445 8.92346 19.1173 9.13931 19.4618 9.51188L19.5612 9.62829C19.8955 10.0523 20.0479 10.6054 19.9868 11.1531L19.1398 18.742C19.0297 19.7286 18.2529 20.4966 17.2964 20.4966H8.69422H5.29398ZM11.9545 6.02658L9.41727 11.7111L9.42149 11.7693L9.42091 19.042H17.2964C17.4587 19.042 17.6222 18.8982 17.6784 18.6701L17.6942 18.5807L18.5412 10.9918C18.5604 10.8194 18.5134 10.6486 18.4189 10.5287C18.3398 10.4284 18.2401 10.378 18.1434 10.378H13.7761C13.3745 10.378 13.0488 10.0524 13.0488 9.65073V7.7535C13.0488 7.2587 12.8749 6.78825 12.5721 6.44915C12.4281 6.28794 12.2615 6.16343 12.0824 6.07923L11.9545 6.02658ZM7.96636 12.4966H5.45455V19.042H7.96636V12.4966Z" fill="white"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M5.29398 20.4966C4.56534 20.4966 4 19.8827 4 19.1539V12.3847C4 11.6559 4.56534 11.042 5.29398 11.042H8.12364L10.8534 4.92738C10.9558 4.69809 11.1677 4.54023 11.4114 4.50434L11.5175 4.49658C12.3273 4.49658 13.0978 4.85402 13.6571 5.48039C14.2015 6.09009 14.5034 6.90649 14.5034 7.7535L14.5027 8.92295L18.1434 8.92346C18.6445 8.92346 19.1173 9.13931 19.4618 9.51188L19.5612 9.62829C19.8955 10.0523 20.0479 10.6054 19.9868 11.1531L19.1398 18.742C19.0297 19.7286 18.2529 20.4966 17.2964 20.4966H8.69422H5.29398ZM11.9545 6.02658L9.41727 11.7111L9.42149 11.7693L9.42091 19.042H17.2964C17.4587 19.042 17.6222 18.8982 17.6784 18.6701L17.6942 18.5807L18.5412 10.9918C18.5604 10.8194 18.5134 10.6486 18.4189 10.5287C18.3398 10.4284 18.2401 10.378 18.1434 10.378H13.7761C13.3745 10.378 13.0488 10.0524 13.0488 9.65073V7.7535C13.0488 7.2587 12.8749 6.78825 12.5721 6.44915C12.4281 6.28794 12.2615 6.16343 12.0824 6.07923L11.9545 6.02658ZM7.96636 12.4966H5.45455V19.042H7.96636V12.4966Z" fill="currentColor"></path></svg>`
 
 export const FormExtension = {
-  name: 'Forms',
-  type: 'response',
-  match: ({ trace }) =>
-    trace.type === 'ext_form' || trace.payload.name === 'ext_form',
-  render: ({ trace, element }) => {
-    const formContainer = document.createElement('form')
-
-    formContainer.innerHTML = `
-          <style>
-            label {
-              font-size: 0.8em;
-              color: #888;
-            }
-            input[type="text"], input[type="email"], input[type="tel"] {
-              width: 100%;
-              border: none;
-              border-bottom: 0.5px solid rgba(0, 0, 0, 0.1);
-              background: transparent;
-              margin: 5px 0;
-              outline: none;
-            }
-            .phone {
-              width: 150px;
-            }
-            .invalid {
-              border-color: red;
-            }
-            .submit {
-              background: linear-gradient(to right, #2e6ee1, #2e7ff1 );
-              border: none;
-              color: white;
-              padding: 10px;
-              border-radius: 5px;
-              width: 100%;
-              cursor: pointer;
-            }
-          </style>
-
-          <label for="name">Name</label>
-          <input type="text" class="name" name="name" required><br><br>
-
-          <label for="email">Email</label>
-          <input type="email" class="email" name="email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" title="Invalid email address"><br><br>
-
-          <label for="phone">Phone Number</label>
-          <input type="tel" class="phone" name="phone" required pattern="\\d+" title="Invalid phone number, please enter only numbers"><br><br>
-
-          <input type="submit" class="submit" value="Submit">
-        `
-
-    formContainer.addEventListener('submit', function (event) {
-      event.preventDefault()
-
-      const name = formContainer.querySelector('.name')
-      const email = formContainer.querySelector('.email')
-      const phone = formContainer.querySelector('.phone')
-
-      if (
-        !name.checkValidity() ||
-        !email.checkValidity() ||
-        !phone.checkValidity()
-      ) {
-        name.classList.add('invalid')
-        email.classList.add('invalid')
-        phone.classList.add('invalid')
-        return
-      }
-
-      formContainer.querySelector('.submit').remove()
-
-      window.voiceflow.chat.interact({
-        type: 'complete',
-        payload: { name: name.value, email: email.value, phone: phone.value },
+    name: 'Forms',
+    type: 'response',
+    match: ({ trace }) =>
+      trace.type === 'ext_form' || trace.payload.name === 'ext_form',
+    render: ({ trace, element }) => {
+      const formContainer = document.createElement('form')
+  
+      formContainer.innerHTML = `
+            <style>
+              label {
+                font-size: 0.8em;
+                color: #888;
+              }
+              input[type="text"], input[type="email"], input[type="tel"], select, textarea {
+                width: 100%;
+                border: none;
+                border-bottom: 0.5px solid rgba(0, 0, 0, 0.1);
+                background: transparent;
+                margin: 5px 0;
+                outline: none;
+              }
+              .phone {
+                width: 150px;
+              }
+              .invalid {
+                border-color: red;
+              }
+              .submit {
+                background: linear-gradient(to right, #2e6ee1, #2e7ff1 );
+                border: none;
+                color: white;
+                padding: 10px;
+                border-radius: 5px;
+                width: 100%;
+                cursor: pointer;
+              }
+            </style>
+  
+            <label for="subject">Subject</label>
+            <select class="subject" name="subject" required>
+              <option value="" disabled selected></option>
+              <option value="Tour Inquiries">Tour Inquiries</option>
+              <option value="Booking Assistance">Booking Assistance</option>
+              <option value="Special Requests">Special Requests</option>
+              <option value="Feedback and Complaints">Feedback and Complaints</option>
+            </select><br><br>
+  
+            <label for="name">Name</label>
+            <input type="text" class="name" name="name" required><br><br>
+  
+            <label for="email">Email</label>
+            <input type="email" class="email" name="email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" title="Invalid email address"><br><br>
+  
+            <label for="phone">Phone Number</label>
+            <input type="tel" class="phone" name="phone" required pattern="\\d+" title="Invalid phone number, please enter only numbers"><br><br>
+  
+            <label for="notes">Notes</label>
+            <textarea class="notes" name="notes"></textarea><br><br>
+  
+            <input type="submit" class="submit" value="Submit">
+          `
+  
+      formContainer.addEventListener('submit', function (event) {
+        event.preventDefault()
+  
+        const subject = formContainer.querySelector('.subject')
+        const name = formContainer.querySelector('.name')
+        const email = formContainer.querySelector('.email')
+        const phone = formContainer.querySelector('.phone')
+        const notes = formContainer.querySelector('.notes')
+  
+        if (
+          !subject.checkValidity() ||
+          !name.checkValidity() ||
+          !email.checkValidity() ||
+          !phone.checkValidity()
+        ) {
+          subject.classList.add('invalid')
+          name.classList.add('invalid')
+          email.classList.add('invalid')
+          phone.classList.add('invalid')
+          return
+        }
+  
+        formContainer.querySelector('.submit').remove()
+  
+        window.voiceflow.chat.interact({
+          type: 'complete',
+          payload: {
+            subject: subject.value,
+            name: name.value,
+            email: email.value,
+            phone: phone.value,
+            notes: notes.value
+          },
+        })
       })
-    })
-
-    element.appendChild(formContainer)
-  },
-}
+  
+      element.appendChild(formContainer)
+    },
+  }
+  
 
 export const MapExtension = {
   name: 'Maps',
